6b9710c339222606b02851667441bff0ce37f9d2
thanhdo <thanhdo@maxflowtech.com>
2024-07-17 15:13:35 +0700

    update MovieList Carausel

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import MovieList from './components/MovieList'
 
 function App() {
   const [movie, setMovie] = useState([])
+  const [movieRate, setMovieRate] = useState([])
 
   useEffect(() => {
     const fetchMovie = async() =>{
@@ -15,10 +16,17 @@ function App() {
           Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`
         }
       };
-      const url = 'https://api.themoviedb.org/3/movie/popular?language=en-vi&page=1';
-      const response = await fetch(url, options);
-      const data = await response.json();
-      console.log(data);
+      const url1 = 'https://api.themoviedb.org/3/movie/popular?language=vi&page=1';
+      const url2 = 'https://api.themoviedb.org/3/movie/top_rated?language=vi&page=1';
+      const [res1, res2] = await Promise.all([
+        fetch(url1, options),
+        fetch(url2, options),
+      ])
+      const data1 = await res1.json();
+      const data2 = await res2.json();
+
+      setMovie(data1.results);
+      setMovieRate(data2.results);
     }
     fetchMovie()
   }, [])
@@ -28,8 +36,8 @@ function App() {
       <div className='bg-black pb-10'>
         <Header />
         <Banner />
-        <MovieList title={"Phim hot"} />
-        <MovieList title={"Phim đề cử"} />
+        <MovieList title={"Phim hot"} data={movie}/>
+        <MovieList title={"Phim đề cử"} data={movieRate}/>
       </div>
     </>
   )
